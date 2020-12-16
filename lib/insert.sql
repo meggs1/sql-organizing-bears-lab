9691e9731d072c9c6377ccdd57ea096e0786957b
meggs1 <megan.schertz@gmail.com>
2020-12-15 21:24:14 -0600

    commit tree 03d674eef4e35e3c6a1b9a8095ca16b9fcb6cf26

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "M", "Brown", "Sweet", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 4, "M", "Black", "Playful", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 60, "F", "White", "Sleepy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 8, "M", "Brown", "Bossy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 1, "F", "White", "Loud", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 5, "M", "Green", "Mean", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 5, "M", "Black", "Sweet", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 3, "M", "Brown", "Murderous", 1);
